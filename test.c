e5d851c5f51cafe9bdd17fc23f6c5ed88f43477e
kevwargo <jarasz.kevwargo@gmail.com>
2015-02-01 03:25:50 +0100

    Maybe last changes

--- test.c
+++ test.c
@@ -31,11 +31,14 @@ void SetThreadAffinityMask(pid_t tid, int cpu)
 
 int main(int argc, char **argv)
 {
-    if (argc < 2)
+    if (argc < 3)
+    {
+        fprintf(stderr, "usage: %s CASE NUM_STEPS\n", argv[0]);
         return 1;
+    }
     double start, total;
 	double pi, sum = 0.0;
-    long long num_steps = 1000 * 1000 * 1000;
+    long long num_steps = atoll(argv[2]);
 	double step = 1. / (double)num_steps;
     int threads_num;
     double *pi_tab;
@@ -86,7 +89,7 @@ int main(int argc, char **argv)
             start = gettime();
 #pragma omp parallel  reduction (+:sum)
             {
-                SetThreadAffinityMask(GetCurrentThread(), omp_get_thread_num() & 1);
+                /* SetThreadAffinityMask(GetCurrentThread(), omp_get_thread_num() & 1); */
 #pragma omp for
                 for (int i = 0; i < num_steps; i++)
                 {
@@ -102,7 +105,7 @@ int main(int argc, char **argv)
             threads_num = 2;
             omp_set_num_threads(threads_num);
             n = 20;
-            pi_tab = malloc(n * sizeof(double));
+            pi_tab = (double *)malloc(n * sizeof(double));
             for (int j = 0; j < n-1; j++)
             {
                 pi_tab[j] = 0;
@@ -111,7 +114,7 @@ int main(int argc, char **argv)
 #pragma omp parallel 
                 {
                     int ind = omp_get_thread_num();
-                    SetThreadAffinityMask(GetCurrentThread(), ind & 1);
+                    /* SetThreadAffinityMask(GetCurrentThread(), ind & 1); */
 #pragma omp for
                     for (int i = 0; i < num_steps; i++)
                     {
@@ -133,7 +136,8 @@ int main(int argc, char **argv)
             start = gettime();
 #pragma omp parallel  reduction (+:sum)
             {
-                SetThreadAffinityMask(GetCurrentThread(), (omp_get_thread_num()%2 +1));
+                /* SetThreadAffinityMask(GetCurrentThread(), 1); */
+                SetThreadAffinityMask(GetCurrentThread(), (omp_get_thread_num() & 1));
 #pragma omp for
                 for (int i = 0; i < num_steps; i++)
                 {
